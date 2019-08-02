aa5d6348ff6b350b60632d188108f6e659fbb4aa
kamida <la5u@u.com>
2019-08-02 17:40:29 +0800

    not knowing how many times

--- src/com/star/TestGit.java
+++ src/com/star/TestGit.java
@@ -8,6 +8,8 @@ public class TestGit {
 		System.out.println("new branch found?");
 		System.out.println("new branch found?");
 		System.out.println("new branch found?");
+		System.out.println("new branch found?");
+		System.out.println("new branch found?");
 		
 	
 	}
