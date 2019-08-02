ac027116d32b4f63e94064ed76aa11423a6f8444
kamida <la5u@u.com>
2019-08-02 17:39:03 +0800

    forgit2 commit

--- src/testGitFlow/Gitflow.java
+++ src/testGitFlow/Gitflow.java
@@ -4,7 +4,11 @@ public class Gitflow {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+		System.out.println("found?");
+		System.out.println("found?");
+		System.out.println("found?");
 	}
 
 }
